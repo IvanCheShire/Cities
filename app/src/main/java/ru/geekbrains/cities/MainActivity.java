7f77859a7b5c104d0d4958a50cbd337986b0cf88
IvanCheShire <ludvigkrauz6@gmail.com>
2020-10-27 14:28:24 +0300

    step3

--- app/src/main/java/ru/geekbrains/cities/MainActivity.java
+++ app/src/main/java/ru/geekbrains/cities/MainActivity.java
@@ -1,25 +1,82 @@
 package ru.geekbrains.cities;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TextView content = findViewById(R.id.content);
+        content.setText(R.string.text_content);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         SocSource sourceData = new SocSource(getResources());
         initRecyclerView(sourceData.build());
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-    private void initRecyclerView(SocSource sourceData){
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_settings:
+                Snackbar.make(toolbar, "Вы выбрали пункт меню установки", Snackbar.LENGTH_LONG)
+                        .setAction("Кнопка", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this.getApplicationContext(), "Кнопка в Snackbar нажата", Toast.LENGTH_LONG).show();
+                            }
+                        }).show();
+                return true;
+            case R.id.action_preferences:
+                Snackbar.make(toolbar, "Вы выбрали пункт меню настройки", Snackbar.LENGTH_LONG).show();
+                return true;
+            case R.id.action_params:
+                Snackbar.make(toolbar, "Вы выбрали пункт меню параметры", Snackbar.LENGTH_LONG).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+        private void initRecyclerView(SocSource sourceData){
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
 
--- app/src/main/java/ru/geekbrains/cities/PicActivity.java
+++ app/src/main/java/ru/geekbrains/cities/PicActivity.java
@@ -1,7 +1,7 @@
 package ru.geekbrains.cities;
 
 import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PicActivity extends AppCompatActivity {
